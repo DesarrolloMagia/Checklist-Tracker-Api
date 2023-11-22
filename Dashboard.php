82f702270910a40a67966dd7f813020210a025ff
Isaac Samaniego <isaac.samaniego@utp.ac.pa>
2023-11-22 00:21:33 -0500

    Cambios Api

--- Dashboard.php
+++ Dashboard.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-
 <html lang="es">
 
 <head>
@@ -12,12 +11,13 @@
 
 <body>
 
-<nav class="navbar">
+    <nav class="navbar">
         <ul class="navbar-list">
-        <li class="navbar-item"><a class="button-link" href="Report.php"></a></li>
-        <li class="navbar-item"><a class="button-link-cerrar" href="Logout.php"></a></li>
+            <li class="navbar-item"><a class="button-link" href="Report.php">Reportes</a></li>
+            <!-- Otros elementos de navegación, si es necesario -->
         </ul>
     </nav>
+
     <div class="container">
         <div class="kanban-head">
             <strong class="kanban-head-title">Mi lista de Control</strong>
@@ -25,7 +25,7 @@
 
         <div class="kanban-table">
             <div class="kanban-form-container">
-            <form action="app/Crear.php" method="POST">
+                <form id="kanban-form" action="app/Crear.php" method="POST">
                     <div class="kanban-form">
                         <strong class="kanban-form-title">Tarea</strong>
                         <div class="container-inputs">
@@ -39,7 +39,7 @@
                             <input type="time" name="hora-culminacion">
                             <strong class="strong-input">Prioridad: </strong>
                             <select name="tarea-prioridad">
-                                <option value="" disabled selected>Seleciones una Prioridad</option>
+                                <option value="" disabled selected>Selecione una Prioridad</option>
                                 <option value="Baja">Baja</option>
                                 <option value="Media">Media</option>
                                 <option value="Alta">Alta</option>
@@ -48,7 +48,7 @@
                             <input type="text" name="tarea-responsable" class="input-text">
                         </div>
                         <?php if (isset($_GET['mess']) && $_GET['mess'] == 'error') { ?>
-                            <p style="color: #ff6666;">Todos los campos son obligatorios.</p>
+                            <p id="error-message" style="color: #ff6666;">Todos los campos son obligatorios.</p>
                         <?php } ?>
                         <input class="btn-crear" id="btn-crear-editar" type="submit" value="Crear Tarea" />
                     </div>
@@ -71,7 +71,7 @@
                         echo "<p>Nombre: " . $tarea['nombre'] . "</p>";
                         echo "<p>Descripción: " . $tarea['descripcion'] . "</p>";
                         echo "<p>Fecha de Compromiso: " . $tarea['fecha_compromiso'] . "</p>";
-                        echo "<p>Hora de Culminación: " . $tarea['hora_culminacion'] . "</p>"; // Muestra la hora de culminación
+                        echo "<p>Hora de Culminación: " . $tarea['hora_culminacion'] . "</p>";
                         echo "<p>Prioridad: " . $tarea['prioridad'] . "</p>";
                         echo "<p>Responsable: " . $tarea['responsable'] . "</p>";
                         if ($tarea['edited'] == 1) {
@@ -101,7 +101,7 @@
                         echo "<p>Nombre: " . $tarea['nombre'] . "</p>";
                         echo "<p>Descripción: " . $tarea['descripcion'] . "</p>";
                         echo "<p>Fecha de Compromiso: " . $tarea['fecha_compromiso'] . "</p>";
-                        echo "<p>Hora de Culminación: " . $tarea['hora_culminacion'] . "</p>"; // Muestra la hora de culminación
+                        echo "<p>Hora de Culminación: " . $tarea['hora_culminacion'] . "</p>";
                         echo "<p>Prioridad: " . $tarea['prioridad'] . "</p>";
                         echo "<p>Responsable: " . $tarea['responsable'] . "</p>";
                         if ($tarea['edited'] == 1) {
@@ -109,7 +109,6 @@
                         }
                         echo "<a href='./app/Edit.php?id=" . $tarea['id'] . "' class='btn-editar'>Editar</a>";
                         echo "<a href='./app/Delete.php?id=" . $tarea['id'] . "' class='btn-eliminar'>Eliminar</a>";
-
                         echo "</div>";
                     }
                 } else {
@@ -128,16 +127,18 @@
 
                 if (count($tareasCompletadas) > 0) {
                     foreach ($tareasCompletadas as $tarea) {
-                        echo "<div class='tarea' draggable='true' ondragstart='drag(event)' id='tarea-" . $tarea['id'] . "'>";
+                        echo "<div class='tarea' draggable='true' ondragstart='drag(event)' id='tarea-" . $tarea['id'] . "' data-estado='" . $tarea['estado'] . "'>";
                         echo "<p>Nombre: " . $tarea['nombre'] . "</p>";
                         echo "<p>Descripción: " . $tarea['descripcion'] . "</p>";
                         echo "<p>Fecha de Compromiso: " . $tarea['fecha_compromiso'] . "</p>";
-                        echo "<p>Hora de Culminación: " . $tarea['hora_culminacion'] . "</p>"; // Muestra la hora de culminación
+                        echo "<p>Hora de Culminación: " . $tarea['hora_culminacion'] . "</p>";
                         echo "<p>Prioridad: " . $tarea['prioridad'] . "</p>";
                         echo "<p>Responsable: " . $tarea['responsable'] . "</p>";
                         if ($tarea['edited'] == 1) {
                             echo "<p><strong>Edited</strong></p>";
                         }
+                        echo "<a href='./app/Edit.php?id=" . $tarea['id'] . "' class='btn-editar'>Editar</a>";
+                        echo "<a href='./app/Delete.php?id=" . $tarea['id'] . "' class='btn-eliminar'>Eliminar</a>";
                         echo "</div>";
                     }
                 } else {
@@ -145,12 +146,13 @@
                 }
                 ?>
             </div>
-
         </div>
     </div>
+
     <script type="text/javascript" charset="utf-8" src="./js/jquery-3.3.1.min.js"></script>
     <script type="text/javascript" charset="utf-8" src="./js/jquery-ui.min.js"></script>
     <script src="./js/script.js"></script>
+    <script src="./js/api.js"></script>
 </body>
+
 </html>
-<?
--- api.php
+++ api.php
@@ -1,8 +1,8 @@
 <?php
 header("Content-Type: application/json");
- 
+
 require '../Connect.php';
- 
+
 // Función para manejar las respuestas
 function sendResponse($status, $message, $data = null)
 {
@@ -14,7 +14,7 @@ function sendResponse($status, $message, $data = null)
     echo json_encode($response);
     exit;
 }
- 
+
 // Endpoint para crear una tarea
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['action']) && $_POST['action'] == 'createTask') {
     $nombre = $_POST['tarea-nombre'];
@@ -23,41 +23,41 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['action']) && $_POST['a
     $hora_culminacion = $_POST['hora-culminacion'];
     $prioridad = $_POST['tarea-prioridad'];
     $responsable = $_POST['tarea-responsable'];
- 
+
     // Validación de campos (puedes agregar más validaciones según sea necesario)
     if (empty($nombre) || empty($fecha_compromiso) || empty($prioridad) || empty($responsable)) {
         sendResponse('error', 'Todos los campos son obligatorios');
     }
- 
+
     $stmt = $conn->prepare("INSERT INTO tareas (nombre, descripcion, fecha_compromiso, hora_culminacion, prioridad, responsable) VALUES (?, ?, ?, ?, ?, ?)");
     $res = $stmt->execute([$nombre, $descripcion, $fecha_compromiso, $hora_culminacion, $prioridad, $responsable]);
- 
+
     if ($res) {
         sendResponse('success', 'Tarea creada con éxito');
     } else {
         sendResponse('error', 'Error al crear la tarea');
     }
 }
- 
+
 // Endpoint para eliminar una tarea
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['action']) && $_POST['action'] == 'deleteTask') {
     $id = $_POST['id'];
- 
+
     // Validación de campos (puedes agregar más validaciones según sea necesario)
     if (!is_numeric($id)) {
         sendResponse('error', 'ID de tarea no válido');
     }
- 
+
     $sql = "DELETE FROM tareas WHERE id = ?";
     $stmt = $conn->prepare($sql);
- 
+
     if ($stmt->execute([$id])) {
         sendResponse('success', 'Tarea eliminada con éxito');
     } else {
         sendResponse('error', 'Error al eliminar la tarea');
     }
 }
- 
+
 // Endpoint para actualizar una tarea
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['action']) && $_POST['action'] == 'updateTask') {
     $id = $_POST['id'];
@@ -66,32 +66,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['action']) && $_POST['a
     $fecha_compromiso = $_POST['fecha-de-compromiso'];
     $prioridad = $_POST['tarea-prioridad'];
     $responsable = $_POST['tarea-responsable'];
- 
+
     // Validación de campos (puedes agregar más validaciones según sea necesario)
     if (!is_numeric($id) || empty($nombre) || empty($fecha_compromiso) || empty($prioridad) || empty($responsable)) {
         sendResponse('error', 'Datos de tarea no válidos');
     }
- 
+
     $sql = "UPDATE tareas SET nombre = ?, descripcion = ?, fecha_compromiso = ?, prioridad = ?, responsable = ?, edited = 1 WHERE id = ?";
     $stmt = $conn->prepare($sql);
- 
+
     if ($stmt->execute([$nombre, $descripcion, $fecha_compromiso, $prioridad, $responsable, $id])) {
         sendResponse('success', 'Tarea actualizada con éxito');
     } else {
         sendResponse('error', 'Error al actualizar la tarea');
     }
 }
- 
+
 // Endpoint para obtener todas las tareas
 if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['action']) && $_GET['action'] == 'getTasks') {
     $sql = "SELECT * FROM tareas";
     $stmt = $conn->prepare($sql);
     $stmt->execute();
     $tareas = $stmt->fetchAll(PDO::FETCH_ASSOC);
- 
+
     sendResponse('success', 'Tareas obtenidas con éxito', $tareas);
 }
- 
+
 // Si no se encuentra ninguna acción válida
 sendResponse('error', 'Acción no válida');
 ?>
